8c3892addc73875e85d47a079872fa9fc57cd1c6
Enes Ahmeti <sundley.12345@gmail.com>
2022-10-11 14:40:54 +0300

    added language support

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
 
 User = get_user_model()
@@ -8,17 +9,17 @@ User = get_user_model()
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField()
+    profile_picture = models.ImageField(_('profile_picture'))
 
     def __str__(self):
         return self.user.username
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=20)
-    subtitle = models.CharField(max_length=20)
-    slug = models.SlugField(unique=True)
-    thumbnail = models.ImageField()
+    title = models.CharField(_('title'), max_length=20)
+    subtitle = models.CharField(_('subtitle'), max_length=20)
+    slug = models.SlugField(_('slug'), unique=True)
+    thumbnail = models.ImageField(_('thumbnail'))
 
     def __str__(self):
         return self.title
@@ -29,15 +30,15 @@ class Category(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    slug = models.SlugField(unique=True)
-    overview = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    content = models.TextField()
+    title = models.CharField(_('title'), max_length=100)
+    slug = models.SlugField(_('slug'), unique=True)
+    overview = models.TextField(_('overview'))
+    timestamp = models.DateTimeField(_('timestamp'), auto_now_add=True)
+    content = models.TextField(_('content'))
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    thumbnail = models.ImageField()
+    thumbnail = models.ImageField(_('thumbnail'))
     categories = models.ManyToManyField(Category)
-    featured = models.BooleanField()
+    featured = models.BooleanField(_('featured'))
 
     def __str__(self):
         return self.title
--- kuzhinablog/urls.py
+++ kuzhinablog/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path("admin/", admin.site.urls),
     path("summernote/", include("django_summernote.urls")),  # Markdown editor
     path("", include('blog.urls'))
-]
+)
 
 
 if settings.DEBUG:
