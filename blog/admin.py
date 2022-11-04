e33baa2da60dc8f625d5b1900b210b79a0b51a90
Enes Ahmeti <sundley.12345@gmail.com>
2022-11-04 15:59:51 +0300

    prepopulates user field when creating post

--- blog/admin.py
+++ blog/admin.py
@@ -7,11 +7,26 @@ class CategoryAdmin(admin.ModelAdmin):
 
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {
+        "slug": ("title", ),
+    }
 
     def get_queryset(self, request):
         qs = self.model.custom_manager.category_by_language
         return qs
+
+    # Removing author field from the admin form.
+    # author should be autoadded.
+    def get_fields(self, request, obj=None):
+        fields = super(PostAdmin, self).get_fields(request, obj)
+        fields.remove("author")
+        return fields
+
+    def save_model(self, request, obj, form, change):
+        # Prepopulates author field.
+        if not change:
+            obj.author = Author.objects.get(user=request.user)
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Post, PostAdmin)
